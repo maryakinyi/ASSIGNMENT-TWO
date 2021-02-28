08b6b3fcaecb8e5ef717929f6fce445d541c119a
maryakinyi <owuorakinyimary@gmail.com>
2021-02-28 17:31:13 +0300

    ASSIGNMENT TWO

---
+++ src/ASSIGNMENT TWO.kt
@@ -0,0 +1,33 @@
+fun main (){
+    name()
+    add()
+    modulus()
+    interesting()
+}
+
+fun add(){
+    var num1=40
+    var num2=50
+    var num3=30
+    var num4=15
+    var sum=num1+num2+num3+num4
+    println(sum)
+
+}
+
+fun name(){
+    var name=("Mary Akinyi")
+    println(name)
+}
+fun modulus() {
+    var num1 = 50
+    var num2 = 30
+    var modulus = num1 % num2
+    println(modulus)
+}
+fun interesting(){
+    var interesting="love reading too much"
+    println(interesting)
+}
+
+
